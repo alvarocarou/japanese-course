1f03b7409ccf7d16dbb6ebd514365fa230678b3a
alvarocarou <49965630+alvarocarou@users.noreply.github.com>
2026-05-03 12:35:16 +0200

    lesson 5

---
+++ Lesson05_Verb_Groups/metadata.js
@@ -0,0 +1,300 @@
+window.CV3_BUNDLE_METADATA = {
+  "title": "Lesson05 Verb Groups",
+  "audio": "audio/final.mp3",
+  "segments": [
+    {
+      "id": "001",
+      "text": "Lesson 5",
+      "time_start": 0.0,
+      "time_end": 0.876458
+    },
+    {
+      "id": "002",
+      "text": "こんにちは。",
+      "time_start": 1.126458,
+      "time_end": 2.406458
+    },
+    {
+      "id": "003",
+      "text": "Today we are going to talk about Japanese verb groups.\nJapanese verbs fall into three groups.\nThese groups only matter when we change the form of a verb but since we do that often they are important to understand.",
+      "time_start": 2.656458,
+      "time_end": 17.496458
+    },
+    {
+      "id": "004",
+      "text": "The three groups are:\n“Group 1”: いちだん verbs\n“Group 2”: ごだん verbs\n“Group 3”: irregular verbs",
+      "time_start": 17.746458,
+      "time_end": 27.906458
+    },
+    {
+      "id": "005",
+      "text": "The first group is called いちだん or “one-level” verbs.\nSome people call them “る-verbs” but a better name would be “いる and える verbs” because these verbs end in いる or える.",
+      "time_start": 28.156458,
+      "time_end": 43.916458
+    },
+    {
+      "id": "006",
+      "text": "They are the simplest kind of verb. When we want to change them we simply remove る and add the new ending.\nFor example:\n“たべる”: “たべて”\n“たべる”: “たべた”",
+      "time_start": 44.166458,
+      "time_end": 60.166458
+    },
+    {
+      "id": "007",
+      "text": "The second group is ごだん verbs or “five-level” verbs.\nThis is the largest group. Japanese verbs always end in a う sound and ごだん verbs can end in many different う sounds.",
+      "time_start": 60.416458,
+      "time_end": 74.496458
+    },
+    {
+      "id": "008",
+      "text": "They can also end in いる or える which is why there can sometimes be confusion with いちだん verbs.\nMost verbs ending in いる or える are いちだん verbs but some are ごだん verbs.",
+      "time_start": 74.746458,
+      "time_end": 91.130708
+    },
+    {
+      "id": "009",
+      "text": "The third group is irregular verbs.",
+      "time_start": 91.380708,
+      "time_end": 93.380708
+    },
+    {
+      "id": "010",
+      "text": "The good news is that Japanese only has two main irregular verbs:\n“くる”: come\n“する”: do",
+      "time_start": 93.630708,
+      "time_end": 105.470708
+    },
+    {
+      "id": "011",
+      "text": "There are a few verbs with small irregularities but very few compared with many European languages.\nNow let’s look at how to make the て-form and the た-form.",
+      "time_start": 105.720708,
+      "time_end": 116.794917
+    },
+    {
+      "id": "012",
+      "text": "As we saw in the previous lesson the て-form is used for structures like the continuous present and the た-form is used for the past tense.",
+      "time_start": 117.044917,
+      "time_end": 129.844917
+    },
+    {
+      "id": "013",
+      "text": "For いちだん verbs this is easy:\n“たべる”: “たべて”\n“たべる”: “たべた”\nYou remove る and add て or た.",
+      "time_start": 130.094917,
+      "time_end": 141.734917
+    },
+    {
+      "id": "014",
+      "text": "ごだん verbs are divided into several ending patterns.",
+      "time_start": 141.984917,
+      "time_end": 145.956458
+    },
+    {
+      "id": "015",
+      "text": "The first pattern is the “うつる” group.\nThis includes verbs ending in:\n“う”\n“つ”\n“る”",
+      "time_start": 146.206458,
+      "time_end": 154.886458
+    },
+    {
+      "id": "016",
+      "text": "To make the て-form we remove the final sound and add small っ plus て.\nFor the た-form we add small っ plus た.",
+      "time_start": 155.136458,
+      "time_end": 166.184667
+    },
+    {
+      "id": "017",
+      "text": "Examples:\n“わらう”: “わらって”: laugh\n“もつ”: “もって”: hold\n“とる”: “とって”: take",
+      "time_start": 166.434667,
+      "time_end": 180.874667
+    },
+    {
+      "id": "018",
+      "text": "So the うつる group uses:",
+      "time_start": 181.124667,
+      "time_end": 183.764667
+    },
+    {
+      "id": "019",
+      "text": "“って”\n“った”",
+      "time_start": 184.014667,
+      "time_end": 186.251833
+    },
+    {
+      "id": "020",
+      "text": "The next group is the “ぬぶむ” group.\nThese verbs end in:\n“ぬ”\n“ぶ”\n“む”",
+      "time_start": 186.501833,
+      "time_end": 192.501833
+    },
+    {
+      "id": "021",
+      "text": "They have a heavier duller sound and their て-form also has a heavier sound:",
+      "time_start": 192.751833,
+      "time_end": 198.874792
+    },
+    {
+      "id": "022",
+      "text": "“んで”",
+      "time_start": 199.124792,
+      "time_end": 200.095333
+    },
+    {
+      "id": "023",
+      "text": "The た-form is:",
+      "time_start": 200.345333,
+      "time_end": 201.946167
+    },
+    {
+      "id": "024",
+      "text": "“んだ”",
+      "time_start": 202.196167,
+      "time_end": 202.703042
+    },
+    {
+      "id": "025",
+      "text": "Examples:\n“しぬ”: “しんで”: “しんだ”: die\n“のむ”: “のんで”: “のんだ”: drink\n“あそぶ”: “あそんで”: “あそんだ”: play",
+      "time_start": 202.953042,
+      "time_end": 216.393042
+    },
+    {
+      "id": "026",
+      "text": "So the ぬぶむ group uses:",
+      "time_start": 216.643042,
+      "time_end": 220.003042
+    },
+    {
+      "id": "027",
+      "text": "“んで”\n“んだ”",
+      "time_start": 220.253042,
+      "time_end": 222.704292
+    },
+    {
+      "id": "028",
+      "text": "The next pattern is the “く and ぐ” group.",
+      "time_start": 222.954292,
+      "time_end": 227.314292
+    },
+    {
+      "id": "029",
+      "text": "For verbs ending in く remove く and add:\n“いて”",
+      "time_start": 227.564292,
+      "time_end": 231.962
+    },
+    {
+      "id": "030",
+      "text": "For the た-form add:\n“いた”",
+      "time_start": 232.212,
+      "time_end": 235.779167
+    },
+    {
+      "id": "031",
+      "text": "Example:\n“あるく”: “あるいて”: “あるいた”: walk",
+      "time_start": 236.029167,
+      "time_end": 242.869167
+    },
+    {
+      "id": "032",
+      "text": "For verbs ending in ぐ the pattern is almost the same but with the voiced sound:\n“いで”\n“いだ”",
+      "time_start": 243.119167,
+      "time_end": 253.599167
+    },
+    {
+      "id": "033",
+      "text": "Example:\n“およぐ”: “およいで”: “およいだ”: swim",
+      "time_start": 253.849167,
+      "time_end": 259.065792
+    },
+    {
+      "id": "034",
+      "text": "So:\n“あるく”: “あるいて”\n“およぐ”: “およいで”",
+      "time_start": 259.315792,
+      "time_end": 264.025542
+    },
+    {
+      "id": "035",
+      "text": "The last regular ごだん pattern is the “す” group.",
+      "time_start": 264.275542,
+      "time_end": 268.515542
+    },
+    {
+      "id": "036",
+      "text": "For verbs ending in す remove す and add:\n“して”",
+      "time_start": 268.765542,
+      "time_end": 274.044917
+    },
+    {
+      "id": "037",
+      "text": "For the た-form add:\n“した”",
+      "time_start": 274.294917,
+      "time_end": 277.974917
+    },
+    {
+      "id": "038",
+      "text": "Example:\n“はなす”: “はなして”: “はなした”: talk",
+      "time_start": 278.224917,
+      "time_end": 283.584917
+    },
+    {
+      "id": "039",
+      "text": "This follows the regular idea of shifting す to its い-row form し.\nNow we have the main ごだん patterns.",
+      "time_start": 283.834917,
+      "time_end": 291.794917
+    },
+    {
+      "id": "040",
+      "text": "There are only three important exceptions:\n“くる”: “きて”: come\n“する”: “して”: do\n“いく”: “いって”: go",
+      "time_start": 292.044917,
+      "time_end": 303.164917
+    },
+    {
+      "id": "041",
+      "text": "Since いく ends in く we might expect it to become “いいて” but it does not. It becomes:\n“いって”",
+      "time_start": 303.414917,
+      "time_end": 311.2445
+    },
+    {
+      "id": "042",
+      "text": "So the key patterns are:",
+      "time_start": 311.4945,
+      "time_end": 312.7745
+    },
+    {
+      "id": "043",
+      "text": "“いちだん verbs”: remove る and add て or た",
+      "time_start": 313.0245,
+      "time_end": 318.012
+    },
+    {
+      "id": "044",
+      "text": "“うつる verbs”: って or った",
+      "time_start": 318.262,
+      "time_end": 321.861583
+    },
+    {
+      "id": "045",
+      "text": "“ぬぶむ verbs”: んで or んだ",
+      "time_start": 322.111583,
+      "time_end": 328.761042
+    },
+    {
+      "id": "046",
+      "text": "“く verbs”: いて or いた\n“ぐ verbs”: いで or いだ",
+      "time_start": 329.011042,
+      "time_end": 336.918042
+    },
+    {
+      "id": "047",
+      "text": "“す verbs”: して or した",
+      "time_start": 337.168042,
+      "time_end": 341.328417
+    },
+    {
+      "id": "048",
+      "text": "“Irregulars”: くる becomes きて する becomes して and いく becomes いって",
+      "time_start": 341.578417,
+      "time_end": 354.322208
+    },
+    {
+      "id": "049",
+      "text": "That is the basic system for Japanese verb groups and the て-form.",
+      "time_start": 354.572208,
+      "time_end": 358.692208
+    }
+  ]
+};
