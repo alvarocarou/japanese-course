01802d6ac9f1e17d3e12584c034f2baf2847b905
alvarocarou <49965630+alvarocarou@users.noreply.github.com>
2026-05-04 18:04:47 +0200

    Add files via upload

---
+++ Lesson06_Adjectives/metadata.js
@@ -0,0 +1,336 @@
+window.CV3_BUNDLE_METADATA = {
+  "title": "Lesson06 Adjectives",
+  "audio": "audio/final.mp3",
+  "segments": [
+    {
+      "id": "001",
+      "text": "Lesson 6",
+      "time_start": 0.0,
+      "time_end": 0.958208
+    },
+    {
+      "id": "002",
+      "text": "こんにちは。",
+      "time_start": 1.208208,
+      "time_end": 1.968208
+    },
+    {
+      "id": "003",
+      "text": "Japanese adjectives are not exactly the same as English adjectives. In Japanese sentences have three basic kinds of engines:",
+      "time_start": 2.218208,
+      "time_end": 11.507958
+    },
+    {
+      "id": "004",
+      "text": "“U-Train”: verb sentences\n“DA-Train”: noun sentences\n“I-Train”: so-called adjective sentences",
+      "time_start": 11.757958,
+      "time_end": 23.463208
+    },
+    {
+      "id": "005",
+      "text": "But the important point is this: all three kinds of engines can be used like adjectives.\nLet’s start with the most obvious one: the い-engine.",
+      "time_start": 23.713208,
+      "time_end": 33.153458
+    },
+    {
+      "id": "006",
+      "text": "A simple sentence is:\n“ぺん が あかい”\nThis means:\n“The pen is red.”",
+      "time_start": 33.403458,
+      "time_end": 38.523458
+    },
+    {
+      "id": "007",
+      "text": "As we know あかい does not simply mean “red”. It means “is-red”.\nNow we can turn あかい into a white engine and put it before ぺん:\n“あかい ぺん”",
+      "time_start": 38.773458,
+      "time_end": 52.217417
+    },
+    {
+      "id": "008",
+      "text": "This means:\n“red pen”\nLiterally it is something like:\n“is-red pen”",
+      "time_start": 52.467417,
+      "time_end": 58.387417
+    },
+    {
+      "id": "009",
+      "text": "But this is no longer a full sentence. A white engine does not pull the train. It only tells us more about the thing after it.",
+      "time_start": 58.637417,
+      "time_end": 67.227917
+    },
+    {
+      "id": "010",
+      "text": "So if we want a full sentence we need a new engine:\n“あかい ぺん が ちいさい”",
+      "time_start": 67.477917,
+      "time_end": 74.277917
+    },
+    {
+      "id": "011",
+      "text": "This means:\n“The red pen is small.”\nちいさい means “is-small”.",
+      "time_start": 74.527917,
+      "time_end": 79.687917
+    },
+    {
+      "id": "012",
+      "text": "Now let’s look at verbs.\nAny verb in any tense can be used like an adjective.\nFor example:\n“しょうじょ が うたった”",
+      "time_start": 79.937917,
+      "time_end": 89.6675
+    },
+    {
+      "id": "013",
+      "text": "This means:\n“The girl sang.”\nうたう means “sing” and うたった is the past form: “sang”.",
+      "time_start": 89.9175,
+      "time_end": 98.4375
+    },
+    {
+      "id": "014",
+      "text": "Now we can turn that verb engine into a white engine and put it before しょうじょ:\n“うたった しょうじょ”",
+      "time_start": 98.6875,
+      "time_end": 106.532
+    },
+    {
+      "id": "015",
+      "text": "This means:\n“the girl who sang”",
+      "time_start": 106.782,
+      "time_end": 109.022
+    },
+    {
+      "id": "016",
+      "text": "Again this is not a full sentence by itself but we can put it into a sentence:\n“うたった しょうじょ が ねている”",
+      "time_start": 109.272,
+      "time_end": 119.992
+    },
+    {
+      "id": "017",
+      "text": "This means:\n“The girl who sang is sleeping.”\nThis is very important because Japanese often works this way. Whole verb sentences can be used adjectivally.",
+      "time_start": 120.242,
+      "time_end": 130.042
+    },
+    {
+      "id": "018",
+      "text": "For example:\n“いぬ が じしょ を たべた”",
+      "time_start": 130.292,
+      "time_end": 132.852
+    },
+    {
+      "id": "019",
+      "text": "This means:\n“The dog ate the dictionary.”",
+      "time_start": 133.102,
+      "time_end": 136.062
+    },
+    {
+      "id": "020",
+      "text": "We can turn it into:\n“じしょ を たべた いぬ”",
+      "time_start": 136.312,
+      "time_end": 139.444
+    },
+    {
+      "id": "021",
+      "text": "This means:\n“the dog who ate the dictionary.”",
+      "time_start": 139.694,
+      "time_end": 142.238625
+    },
+    {
+      "id": "022",
+      "text": "Or:\n“いぬ が たべた じしょ”",
+      "time_start": 142.488625,
+      "time_end": 148.187458
+    },
+    {
+      "id": "023",
+      "text": "This means:\n“the dictionary that the dog ate.”",
+      "time_start": 148.437458,
+      "time_end": 151.019208
+    },
+    {
+      "id": "024",
+      "text": "Then we can build a full sentence:\n“じしょ を たべた いぬ が やんちゃ だ”",
+      "time_start": 151.269208,
+      "time_end": 156.808875
+    },
+    {
+      "id": "025",
+      "text": "This means:\n“The dog who ate the dictionary is naughty.”\nやんちゃ is a noun meaning “naughty” or “bad”.",
+      "time_start": 157.058875,
+      "time_end": 164.578875
+    },
+    {
+      "id": "026",
+      "text": "This brings us to noun engines.\nA simple noun sentence is:\n“いぬ が やんちゃ だ”",
+      "time_start": 164.828875,
+      "time_end": 171.628875
+    },
+    {
+      "id": "027",
+      "text": "This means:\n“The dog is naughty.”\nBut we can also turn this noun engine into a white engine. When だ or です connects to something it changes to な.",
+      "time_start": 171.878875,
+      "time_end": 187.370333
+    },
+    {
+      "id": "028",
+      "text": "So:\n“いぬ が やんちゃ だ”\nbecomes:\n“やんちゃ な いぬ”",
+      "time_start": 187.620333,
+      "time_end": 191.100333
+    },
+    {
+      "id": "029",
+      "text": "This means:\n“the naughty dog”\nLiterally it is like:\n“the dog that is naughty”",
+      "time_start": 191.350333,
+      "time_end": 197.910333
+    },
+    {
+      "id": "030",
+      "text": "Then we can make a full sentence:\n“やんちゃ な いぬ が ねている”",
+      "time_start": 198.160333,
+      "time_end": 202.640333
+    },
+    {
+      "id": "031",
+      "text": "This means:\n“The naughty dog is sleeping.”",
+      "time_start": 202.890333,
+      "time_end": 205.690333
+    },
+    {
+      "id": "032",
+      "text": "These are what textbooks often call “な-adjectives”. But they are really nouns that can be used adjectivally with な.\nHowever not every noun works this way. Only certain nouns are commonly used with な.",
+      "time_start": 205.940333,
+      "time_end": 218.340333
+    },
+    {
+      "id": "033",
+      "text": "Can other nouns be used adjectivally?\nYes but in a different way. For that we use の.\nの is a very simple particle. It works a lot like apostrophe-s in English.",
+      "time_start": 218.590333,
+      "time_end": 229.830333
+    },
+    {
+      "id": "034",
+      "text": "For example:\n“さくら の どれす”",
+      "time_start": 230.080333,
+      "time_end": 232.604667
+    },
+    {
+      "id": "035",
+      "text": "This means:\n“Sakura’s dress.”",
+      "time_start": 232.854667,
+      "time_end": 234.937708
+    },
+    {
+      "id": "036",
+      "text": "“わたし の はな”\nThis means:\n“my nose.”",
+      "time_start": 235.187708,
+      "time_end": 238.603125
+    },
+    {
+      "id": "037",
+      "text": "Japanese does not need separate words like “my” “your” “his” or “her” in the same way English does. It can use の.\nBecause の shows possession or belonging it can also show that something belongs to a certain class or group.",
+      "time_start": 238.853125,
+      "time_end": 255.093125
+    },
+    {
+      "id": "038",
+      "text": "For example:\n“がっこう の せんせい です”",
+      "time_start": 255.343125,
+      "time_end": 258.615042
+    },
+    {
+      "id": "039",
+      "text": "This means:\n“I am a school teacher.”",
+      "time_start": 258.865042,
+      "time_end": 260.785042
+    },
+    {
+      "id": "040",
+      "text": "Literally it means something like:\n“I am a teacher belonging to the school.”\nHere の shows that the teacher belongs to or is connected with the school.",
+      "time_start": 261.035042,
+      "time_end": 271.045583
+    },
+    {
+      "id": "041",
+      "text": "For example あかい means “red” because あか can become the い-form あかい.\nBut not all colors work that way.",
+      "time_start": 271.295583,
+      "time_end": 282.695583
+    },
+    {
+      "id": "042",
+      "text": "“ぴんくいろ” means “pink”.\nいろ means “color”.",
+      "time_start": 282.945583,
+      "time_end": 288.449167
+    },
+    {
+      "id": "043",
+      "text": "But ぴんくいろ does not have an い-form and it is not usually treated as a な-adjective either.",
+      "time_start": 288.699167,
+      "time_end": 294.423292
+    },
+    {
+      "id": "044",
+      "text": "So we use の:\n“ぴんくいろ の どれす”",
+      "time_start": 294.673292,
+      "time_end": 298.746375
+    },
+    {
+      "id": "045",
+      "text": "This means:\n“pink dress.”\nLiterally it means something like:\n“a dress belonging to the class of pink-colored things.”",
+      "time_start": 298.996375,
+      "time_end": 307.076375
+    },
+    {
+      "id": "046",
+      "text": "Another example:\n“うさぎ の おすかる”",
+      "time_start": 307.326375,
+      "time_end": 310.663667
+    },
+    {
+      "id": "047",
+      "text": "This means:\n“Oscar the Rabbit.”\nLiterally it is like:\n“Oscar belonging to the class rabbit.”",
+      "time_start": 310.913667,
+      "time_end": 318.353667
+    },
+    {
+      "id": "048",
+      "text": "Other examples:\n“ぜるだ の でんせつ”: “The Legend of Zelda”",
+      "time_start": 318.603667,
+      "time_end": 322.603667
+    },
+    {
+      "id": "049",
+      "text": "“でんせつ の せんし”: “legendary warrior”",
+      "time_start": 322.853667,
+      "time_end": 327.052667
+    },
+    {
+      "id": "050",
+      "text": "So Japanese has four main ways of forming adjectival expressions:",
+      "time_start": 327.302667,
+      "time_end": 331.982667
+    },
+    {
+      "id": "051",
+      "text": "“い-engine”: あかい ぺん\n“verb engine”: うたった しょうじょ\n“noun engine with な”: やんちゃ な いぬ\n“noun with の”: ぴんくいろ の どれす",
+      "time_start": 332.232667,
+      "time_end": 350.281333
+    },
+    {
+      "id": "052",
+      "text": "This lets Japanese build very complex sentences especially with verb-based adjectivals where whole sentences can describe a noun.\nYou might wonder whether you need to memorize which nouns use な and which use の.",
+      "time_start": 350.531333,
+      "time_end": 366.145542
+    },
+    {
+      "id": "053",
+      "text": "Unless you are studying for an exam it is probably not necessary to memorize long lists at the beginning.\nIf you hear them you will understand them. If you use the wrong one people will usually still understand you and it is a small common learner mistake.",
+      "time_start": 366.395542,
+      "time_end": 382.315542
+    },
+    {
+      "id": "054",
+      "text": "When writing you can look them up. And as you read listen and use more Japanese you will naturally get used to which ones take な and which ones take の.",
+      "time_start": 382.565542,
+      "time_end": 394.752208
+    },
+    {
+      "id": "055",
+      "text": "Japanese is not just a game of memorizing abstract rules. It is a language that can be learned naturally and understanding its real structure makes that much easier.",
+      "time_start": 395.002208,
+      "time_end": 406.402208
+    }
+  ]
+};
